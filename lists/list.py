9b05c7519ed689e855657749036447fbb61c9a66
Sameer Dey <kalantrilonda@gmail.com>
2023-07-25 09:07:08 +0530

    list updated

---
+++ lists/list.py
@@ -0,0 +1,26 @@
+x = list(range(1,11))
+
+# accessing the values of list
+# print(x[0]) 
+# print(x[2]) 
+# print(x[4]) 
+# loops 
+
+# for loops 
+# for num in x:
+#     print(num)
+
+
+# while loop 
+# i=0
+# while i <len(x):
+#     print(x[i],end='')
+#     i +=1
+
+# list methods 
+# x.append(11)
+# x.extend([11,12,13,14,15])
+# x.append([11,12,13,14,15])
+
+# x.insert(5,"hello")
+print(x)
