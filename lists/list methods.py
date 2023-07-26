64428fd87800d6ffc0afd2157f3ea772344de64e
Sameer Dey <kalantrilonda@gmail.com>
2023-07-26 10:06:01 +0530

    list methods updated

---
+++ lists/list methods.py
@@ -0,0 +1,27 @@
+# Remove the item from list 
+# x.clear()
+# x.pop(5)
+# x.remove(19)
+# y = " ".join(x)
+# x.sort()
+# x = ["Tajmahal", "is", "in", "Agra"]
+# x = [4,9,67,87,3,1,345,345,345,345]
+# y = x.index(99)
+# print(y)
+
+# y = x[0:]
+# # print(x is y)
+# print(y)
+
+
+
+
+
+# num = list(range(1,6))
+
+# num[1:3] = ["Khushi", "Manjeet","c",True]
+# print(num)
+
+x = "Coding is fun"
+y = x[::-1]
+print(y)
